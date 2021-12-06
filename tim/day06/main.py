32abd75d4bcd21069ca699f9a8da155f0c3d30c9
TimHasert <27998143+TimH96@users.noreply.github.com>
2021-12-06 17:30:44 +0100

    added copy to not mess with original list

--- tim/day06/main.py
+++ tim/day06/main.py
@@ -1,7 +1,10 @@
+from collections import deque
+
+
 def problem1(swarm: list[int], iterations: int) -> int:
     # this solution is O(n*k) in time and something truly ungodly in space complexity
     # you could just replace problem1 with problem2 solution entirely, i'm keeping it here to own up to it
-    sol: list[int] = swarm
+    sol: list[int] = [x for x in swarm]  # copy because python is funny :))))
     for _ in range(iterations):
         for i in range(len(sol)):
             if sol[i] == 0:
@@ -12,24 +15,18 @@ def problem1(swarm: list[int], iterations: int) -> int:
     return len(sol)
 
 
-def problem2(fish: int, iterations: int) -> int:
-    def single_fish(fish: int, iterations: int) -> int:
-        offspawn: int = 0
-        remaining: int = iterations
-        while True:
-            if fish > remaining:
-                return 1
-            else:
-                offspawn += single_fish(8, remaining)
-                remaining -= 6
+def problem2(swarm: list[int], iterations: int) -> int:
+    fishes: deque[int] = deque([0] * 9)
+    for fish in swarm:
+        fishes[fish] += 1
+    for _ in range(iterations):
+        fishes[7] += fishes[0]
+        fishes.rotate(-1)
+    return sum(fishes)
     
-    return single_fish(fish, iterations)
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data: list[int] = [int(x) for x in f.read().split(",")]
-    # print("solution 1:", problem1(data, 80))
-    # print("solution 2:", problem1(3, 256))
-    bla = [0]
-    print(problem1(bla, 80))
-    print(problem2(3, 11))
+    print("solution 1:", problem1(data, 80))
+    print("solution 2:", problem2(data, 256))
