6795c218476c37f9a8f75900961649b5b9dfe5ad
TimHasert <timhasert@gmail.com>
2021-12-06 11:53:13 +0100

    day05 problem2

--- tim/day05/main.py
+++ tim/day05/main.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from datapoint import DataPoint
 
 
@@ -22,8 +23,40 @@ def problem1(data: list[tuple[DataPoint, DataPoint]], grid_size: int = 1000):
     return counter
 
 
-def problem2():
-    pass
+def problem2(data: list[tuple[DataPoint, DataPoint]], grid_size: int = 1000):
+    grid: list[list[int]] = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+    counter: int = 0
+
+    def add_point(x, y) -> None:
+        nonlocal counter
+        grid[x][y] += 1
+        if grid[x][y] == 2:
+            counter += 1
+
+    # vertical
+    for (p1, p2) in [pair for pair in data if pair[0].x == pair[1].x]:
+        for y in range(min(p1.y, p2.y), max(p1.y, p2.y) + 1):
+            add_point(p1.x, y)
+    # horizontal
+    for (p1, p2) in [pair for pair in data if pair[0].y == pair[1].y]:
+        for x in range(min(p1.x, p2.x), max(p1.x, p2.x) + 1):
+            add_point(x, p1.y)
+    # diagonal
+    for (p1, p2) in [
+        pair
+        for pair in data
+        if abs(pair[0].y - pair[1].y) == abs(pair[0].x - pair[1].x)
+    ]:
+        start: DataPoint = min([p1, p2], key=lambda p: p.x)
+        stop: DataPoint = max([p1, p2], key=lambda p: p.x)
+        direction_function: Callable = (
+            (lambda y, step: y + step)
+            if start.y < stop.y
+            else (lambda y, step: y - step)
+        )
+        for i, x in enumerate(range(start.x, stop.x + 1)):
+            add_point(x, direction_function(start.y, i))
+    return counter
 
 
 if __name__ == "__main__":
@@ -35,4 +68,4 @@ if __name__ == "__main__":
             ]
         ]
     print("solution 1:", problem1(data))
-    # print("solution 2:", problem2(data))
+    print("solution 2:", problem2(data))
