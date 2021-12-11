4adba5e08bd3549399b88c1a75761fdec6d08883
TimHasert <27998143+TimH96@users.noreply.github.com>
2021-12-11 12:20:21 +0100

    made day08 more compact

--- tim/day08.py
+++ tim/day08.py
@@ -15,30 +15,20 @@ def problem2(signals: list[tuple[str, str]]) -> int:
         o: list[str] = raw_signal[1].split()
 
         # find the unique length numbers
-        numbers[1] = set([x for x in i if len(x) == 2][0])
-        numbers[4] = set([x for x in i if len(x) == 4][0])
-        numbers[7] = set([x for x in i if len(x) == 3][0])
-        numbers[8] = set([x for x in i if len(x) == 7][0])
+        for (n, leng) in [(1, 2), (4, 4), (7, 3), (8, 7)]:
+            numbers[n] = set([x for x in i if len(x) == leng][0])
 
-        # find the len6 numbers
-        for num in [set(x) for x in i if len(x) == 6]:
-            if len(num - numbers[1]) == 4:
-                if len(num - numbers[4]) == 2:
-                    numbers[9] = num
+        # find numbers of leng5 and leng6
+        # both follow the same logic of comparing difference in segments between num1 and num4
+        for (leng, n1, n2, n3) in [(6, 9, 0, 6), (5, 5, 2, 3)]:
+            for num in [set(x) for x in i if len(x) == leng]:
+                if len(num - numbers[1]) == 4:
+                    if len(num - numbers[4]) == 2:
+                        numbers[n1] = num
+                    else:
+                        numbers[n2] = num
                 else:
-                    numbers[0] = num
-            else:
-                numbers[6] = num
-
-        # find the len5 numbers
-        for num in [set(x) for x in i if len(x) == 5]:
-            if len(num - numbers[1]) == 4:
-                if len(num - numbers[4]) == 2:
-                    numbers[5] = num
-                else:
-                    numbers[2] = num
-            else:
-                numbers[3] = num
+                    numbers[n3] = num
 
         # calculate output of this signal
         # you could definitely make this less time complex but at point i dont care
